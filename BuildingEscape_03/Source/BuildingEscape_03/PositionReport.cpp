e118cc5bf78cce9582cf940538fd354848f9ed2d
Thebestbmxer <etvanosdol@hotmail.com>
2017-03-19 10:42:06 -0400

    Up to Open Door

--- BuildingEscape_03/Source/BuildingEscape_03/PositionReport.cpp
+++ BuildingEscape_03/Source/BuildingEscape_03/PositionReport.cpp
@@ -3,15 +3,12 @@
 #include "BuildingEscape_03.h"
 #include "PositionReport.h"
 
-
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -20,8 +17,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 }
 
 
